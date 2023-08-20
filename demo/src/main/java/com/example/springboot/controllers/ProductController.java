26021bca88d5ea46ab3cf2493304a5e76734e4e0
Gabriel.M <gamotasantos2000@gmail.com>
2023-08-20 19:38:21 -0400

    Method PUT ok

--- demo/src/main/java/com/example/springboot/controllers/ProductController.java
+++ demo/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,5 +47,26 @@ public class ProductController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(product0.get());
     }
+
+    @PutMapping("products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value="id") UUID id, 
+                                                @RequestBody @Valid ProductRecordDto productRecordDto){
+
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if(product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        var productModel = product0.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    
+                                                
+                                                
+                                                
+                                                
+                                                
+                                                
+    }
+
 
 }
